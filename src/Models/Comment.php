c8fd3be0ee09e4356554c5c10ecce7b55db36825
Matilde Enevoldsen <matildeene@outlook.com>
2024-06-16 18:16:49 +0100

    update version

--- src/Models/Comment.php
+++ src/Models/Comment.php
@@ -21,7 +21,7 @@ class Comment extends Model
 
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(config('auth.providers.users.model', App\Models\User::class));
     }
 
     public function children(): HasMany
