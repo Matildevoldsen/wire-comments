efc4be4855bd5cd66d47e67fca872bef467317b2
Matilde Enevoldsen <matildeene@outlook.com>
2024-06-16 20:10:59 +0100

    remove prefix

--- config/wire-comments.php
+++ config/wire-comments.php
@@ -1,19 +1,5 @@
 <?php
 
 return [
-    /**
-     * Default is empty.
-     *    prefix => ''
-     *              <x-button />
-     *              <x-card />
-     *
-     * Renaming all components:
-     *    prefix => 'wire-comments-'
-     *               <x-wire-comments-button />
-     *               <x-wire-comments-card />
-     *
-     * Make sure to clear view cache after renaming
-     *    php artisan view:clear
-     */
-    'prefix' => '',
+
 ];
