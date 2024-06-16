47e3714afffd98e93426f67f9697147103a5cc49
Matilde Enevoldsen <matildeene@outlook.com>
2024-06-16 17:22:50 +0100

    add models

---
+++ src/Models/Comment.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace WireComments\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Comment extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'body',
+        'parent_id'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function children(): HasMany
+    {
+        return $this->hasMany(Comment::class, 'parent_id', 'id');
+    }
+
+    public function reactions(): HasMany
+    {
+        return $this->hasMany(Reaction::class);
+    }
+}
---
+++ src/Models/Reaction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace WireComments\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Reaction extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'user_id',
+        'comment_id',
+        'emoji'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comment(): BelongsTo
+    {
+        return $this->belongsTo(Comment::class);
+    }
+}
